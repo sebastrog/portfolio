b7014cb7e4c719ce2bb1da97c2c0a2948d054bc2
chomapublicize <choma@publicize.co>
2024-01-03 12:04:52 -0500

    deploys

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const isProd = import.meta.env.PROD;
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://sebastrog.github.io",
+  base: isProd ? "/portfolio" : "",
+  output: "static",
+  build: {
+    assets: 'assets'
+  }
 });
+
