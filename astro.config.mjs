052e653046b74809bd84e7d4ef44b0e9c95f075d
sebastrog <sebastiancg1070@gmail.com>
2025-09-07 00:11:17 -0500

    work in progress

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "IBM Plex Mono",
+        cssVariable: "--font-ibm",
+        weights: [200, 300, 400, 500, 600, 700],
+      },
+    ],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
